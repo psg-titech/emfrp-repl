b51c66ef3fdc9c94e35a7c6ccec2002435999f46
Go Suzuki <puyogo.suzuki@gmail.com>
2023-01-14 15:05:25 +0900

    [FIX][gui] Support the new API.

--- gui/emfrp-repl-gui.py
+++ gui/emfrp-repl-gui.py
@@ -1,7 +1,7 @@
 
 # Title: Emfrp repl GUI frontend(Python/tk)
 # Author: Go Suzuki <puyogo.suzuki@gmail.com>
-# Date: 2022/10/15
+# Date: 2023/1/14
 
 import tkinter
 import ctypes
@@ -25,21 +25,23 @@ elif os.name == 'posix':
         print('Not tested platform!')
 
 libemfrp = ctypes.cdll.LoadLibrary(librarypath)
-libemfrp.emfrp_create.restype = ctypes.c_void_p
-libemfrp.emfrp_create.argtypes = []
-libemfrp.emfrp_repl.restype = ctypes.c_bool
+libemfrp.emfrp_create.restype = ctypes.c_int32
+libemfrp.emfrp_create.argtypes = [ctypes.c_void_p]
+libemfrp.emfrp_repl.restype = ctypes.c_int32
 libemfrp.emfrp_repl.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_void_p]
-libemfrp.emfrp_add_input_node_definition.restype = ctypes.c_bool
-libemfrp.emfrp_add_input_node_definition.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_void_p]
-libemfrp.emfrp_indicate_node_update.restype = ctypes.c_bool
-libemfrp.emfrp_indicate_node_update.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_void_p]
-libemfrp.emfrp_add_output_node_definition.restype = ctypes.c_bool
-libemfrp.emfrp_add_output_node_definition.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_void_p]
+libemfrp.emfrp_add_input_node.restype = ctypes.c_int32
+libemfrp.emfrp_add_input_node.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_void_p]
+libemfrp.emfrp_update.restype = ctypes.c_int32
+libemfrp.emfrp_update.argtypes = [ctypes.c_void_p]
+libemfrp.emfrp_add_output_node.restype = ctypes.c_int32
+libemfrp.emfrp_add_output_node.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_void_p]
 libemfrp.emfrp_create_int_object.restype = ctypes.c_void_p
 libemfrp.emfrp_create_int_object.argtypes = [ctypes.c_int32]
 libemfrp.emfrp_get_integer.restype = ctypes.c_int32
 libemfrp.emfrp_get_integer.argtypes = [ctypes.c_void_p]
-emfrp_t = libemfrp.emfrp_create()
+libemfrp.emfrp_print_object.argstype = [ctypes.c_void_p]
+emfrp_t = ctypes.c_void_p(0)
+libemfrp.emfrp_create(ctypes.pointer(emfrp_t))
 root = tkinter.Tk()
 root.title('emfrp-repl')
 
@@ -56,7 +58,8 @@ def execCommand():
     logText['state'] = tkinter.NORMAL
     logText.insert('end', '\n' + s)
     logText.insert('end', '\nReturn Code:' + str(returnCode))
-    logText.insert('end', '\nReturn Value:' + str(libemfrp.emfrp_get_integer(o)))
+    libemfrp.emfrp_print_object(o)
+#    logText.insert('end', '\nReturn Value:' + str(libemfrp.emfrp_get_integer(o)))
     logText['state'] = tkinter.DISABLED
     commandVar.set('')
 
@@ -88,12 +91,12 @@ def addInputNode():
     GET_CALLBACK = ctypes.CFUNCTYPE(ctypes.c_void_p)
     # dont collect by gc!
     val.get_callback = GET_CALLBACK(inputNodeGet)
-    libemfrp.emfrp_add_input_node_definition(emfrp_t, ctypes.c_char_p(bytes(inputNodeNumStr, 'ascii')), val.get_callback)
+    logText.insert('end', '\nReturn Code:' + str(libemfrp.emfrp_add_input_node(emfrp_t, ctypes.c_char_p(bytes(inputNodeNumStr, 'ascii')), val.get_callback)))
     la = tkinter.Label(cont,textvariable=txtVal)
     def inputNodeSliderChanged(a):
         txtVal.set(inputNodeNumStr + ' : ' + str(val.get()))
-        libemfrp.emfrp_indicate_node_update(emfrp_t, ctypes.c_char_p(bytes(inputNodeNumStr, 'ascii')),
-                                            libemfrp.emfrp_create_int_object(val.get()))
+        libemfrp.emfrp_set_node_value(emfrp_t, ctypes.c_char_p(bytes(inputNodeNumStr, 'ascii')), libemfrp.emfrp_create_int_object(val.get()))
+        libemfrp.emfrp_update(emfrp_t)
     tkinter.Scale(
         cont,
         variable=val,
@@ -131,8 +134,8 @@ def addOutputNode():
     INDICATE_CALLBACK = ctypes.CFUNCTYPE(None, ctypes.c_void_p)
     # dont collect by gc!
     txtVal.indicate_callback = INDICATE_CALLBACK(indicated)
-    print(libemfrp.emfrp_add_output_node_definition(emfrp_t, ctypes.c_char_p(bytes(outputNodeNumStr, 'ascii')),
-                                              txtVal.indicate_callback))
+    logText.insert('end', '\nReturn Code:' + str(libemfrp.emfrp_add_output_node(emfrp_t, ctypes.c_char_p(bytes(outputNodeNumStr, 'ascii')),
+                                              txtVal.indicate_callback)))
     la = tkinter.Label(cont, textvariable=txtVal)
     la.pack()
     outputNodeNum = outputNodeNum + 1
